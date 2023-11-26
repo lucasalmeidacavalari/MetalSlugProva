22895dbb4a116ae3564f5ae0b2014bcf38ee223a
Lucas Cavalari <lucas.eduardo1203@hotmail.com>
2023-11-26 18:54:03 -0300

    finalizando projeto

---
+++ Assets/Scripts/TweenRot.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.UI;
+using DG.Tweening;
+
+public class TweenZoom : MonoBehaviour
+{
+
+    [SerializeField]
+    private Image imgZoom;
+    [SerializeField]
+    private bool liberaZoom = true;
+    [SerializeField]
+    private float zoomInScale = 1.5f; // Escala para o zoom in
+    [SerializeField]
+    private float zoomDuration = 1f; // Duração do zoom
+
+    void Start()
+    {
+        StartCoroutine(ZoomInOut());
+    }
+
+    IEnumerator ZoomInOut()
+    {
+        while (liberaZoom)
+        {
+            // Zoom in
+            imgZoom.rectTransform.DOScale(zoomInScale, zoomDuration);
+            yield return new WaitForSeconds(zoomDuration);
+
+            // Zoom out
+            imgZoom.rectTransform.DOScale(1f, zoomDuration);
+            yield return new WaitForSeconds(zoomDuration);
+        }
+    }
+}
